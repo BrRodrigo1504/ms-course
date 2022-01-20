8872817ef6f78d61df1c483a63147f446e5eb9a6
Rodrigo Fernandes <rodrigops1504@gmail.com>
2022-01-20 22:57:37 +0000

    Private Git Repository

--- hr-worker/src/main/java/com/rodrigo/hrworker/entities/Worker.java
+++ hr-worker/src/main/java/com/rodrigo/hrworker/entities/Worker.java
@@ -1,7 +1,6 @@
 package com.rodrigo.hrworker.entities;
 
 import java.io.Serializable;
-import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -18,16 +17,16 @@ public class Worker implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
-	private Double DAILY_INCOME;
+	private Double dailyIncome;
 
 	public Worker() {
 	}
 
-	public Worker(Long id, String name, Double dAILY_INCOME) {
+	public Worker(Long id, String name, Double dailyIncome) {
 		super();
 		this.id = id;
 		this.name = name;
-		DAILY_INCOME = dAILY_INCOME;
+		this.dailyIncome = dailyIncome;
 	}
 
 	public Long getId() {
@@ -46,17 +45,20 @@ public class Worker implements Serializable {
 		this.name = name;
 	}
 
-	public Double getDAILY_INCOME() {
-		return DAILY_INCOME;
+	public Double getDailyIncome() {
+		return dailyIncome;
 	}
 
-	public void setDAILY_INCOME(Double dAILY_INCOME) {
-		DAILY_INCOME = dAILY_INCOME;
+	public void setDailyIncome(Double dailyIncome) {
+		this.dailyIncome = dailyIncome;
 	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -68,7 +70,11 @@ public class Worker implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Worker other = (Worker) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
-
 }
