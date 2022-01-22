773eb795defcca2e3a63320fea4abf54f8381e51
Rodrigo Fernandes <rodrigops1504@gmail.com>
2022-01-22 12:51:06 +0000

    hr-user Postgres dev Profile

--- hr-user/src/main/java/com/example/hruser/entities/User.java
+++ hr-user/src/main/java/com/example/hruser/entities/User.java
@@ -2,7 +2,6 @@ package com.example.hruser.entities;
 
 import java.io.Serializable;
 import java.util.HashSet;
-import java.util.Objects;
 import java.util.Set;
 
 import javax.persistence.Column;
@@ -29,16 +28,15 @@ public class User implements Serializable {
 	@Column(unique = true)
 	private String email;
 	private String password;
-
+	
 	@ManyToMany(fetch = FetchType.EAGER)
 	@JoinTable(name = "tb_user_role",
 		joinColumns = @JoinColumn(name = "user_id"),
-		inverseJoinColumns = @JoinColumn(name = "role_id")
+		inverseJoinColumns = @JoinColumn(name = "role_id")			
 	)
 	private Set<Role> roles = new HashSet<>();
-
+	
 	public User() {
-
 	}
 
 	public User(Long id, String name, String email, String password) {
@@ -87,7 +85,10 @@ public class User implements Serializable {
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -99,7 +100,11 @@ public class User implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		User other = (User) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
-
 }
--- hr-user/src/main/java/com/example/hruser/resources/UserResource.java
+++ hr-user/src/main/java/com/example/hruser/resources/UserResource.java
@@ -14,15 +14,15 @@ import com.example.hruser.repositories.UserRepository;
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
-
+	
 	@Autowired
 	private UserRepository repository;
-
+	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<User> findById(@PathVariable Long id) {
 		User obj = repository.findById(id).get();
 		return ResponseEntity.ok(obj);
-	}
+	}	
 	
 	@GetMapping(value = "/search")
 	public ResponseEntity<User> findByEmail(@RequestParam String email) {
